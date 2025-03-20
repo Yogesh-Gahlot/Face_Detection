e50873d554e7e729b33d2e197da102ed8040a4f1
Yogesh Gahlot <yogeshgahlot100@gmail.com>
2025-03-20 11:48:38 +0530

    uploading flask project

--- Face_tracking.py
+++ Face_tracking.py
@@ -1,36 +1,46 @@
-import cv2 as cv 
-import mediapipe as mp 
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+import base64
+import io
+from PIL import Image
 
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-mp_drawing =mp.solutions.drawing_utils
-mp_face= mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+mp_face = mp.solutions.face_mesh
 
-drawing_spec= mp_drawing.DrawingSpec(thickness=1,circle_radius= 1)
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-video =cv.VideoCapture(0)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-with mp_face.FaceMesh(min_detection_confidence=0.5,
-                min_tracking_confidence=0.5) as face_mesh:
+@socketio.on('frame')
+def handle_frame(data):
+    img_data = base64.b64decode(data.split(',')[1])
+    img = Image.open(io.BytesIO(img_data))
+    frame = np.array(img)
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
     
-    while True:
-        ret, image= video.read()
-        image =cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        image.flags.writeable= False
-        results= face_mesh.process(image)
-        image.flags.writeable=True
-        
-        image =cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    with mp_face.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
+        results = face_mesh.process(frame)
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(image= image , landmark_list= face_landmarks,
-                                            connections= mp_face.FACEMESH_TESSELATION ,
-                                            landmark_drawing_spec= drawing_spec,
-                                            connection_drawing_spec= drawing_spec)
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=drawing_spec,
+                    connection_drawing_spec=drawing_spec
+                )
     
-        cv.imshow("Face Mesh",image)
-        k= cv.waitKey(1)
-        if k != -1:
-            break
+    _, buffer = cv.imencode('.jpg', frame)
+    encoded_frame = base64.b64encode(buffer).decode('utf-8')
+    emit('processed_frame', f'data:image/jpeg;base64,{encoded_frame}')
 
-video.release()
-cv.destroyAllWindows()
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
